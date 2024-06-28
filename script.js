25cd438f543b492050f972f28345cdb9323cc5b8
Stanley Clem <ukoerhistanley@gmail.com>
2024-06-28 16:41:13 +0100

    MY FAVOURITE SPORTS

--- script.js
+++ script.js
@@ -1,30 +1,63 @@
-let count = 0;
+// let count = 0;
 
-const wheel = document.querySelector(".spin")
+// const wheel = document.querySelector(".spin")
 
-spin();
+// spin();
 
-function spin() {
-  count++;
-  // wheel.style.translate = `150px`
-  // wheel.style.transform = `rotate(${count}deg)`
-  setTimeout(spin, 30);
-}
+// function spin() {
+//   count++;
+//   // wheel.style.translate = `150px`
+//   // wheel.style.transform = `rotate(${count}deg)`
+//   setTimeout(spin, 30);
+// }
+
+// let index = 0;
+// const containerE = document.querySelector('.cont');
+// const stan = `Thank you lord for providing for me`
+
+// kiss();
+
+// function kiss() {
+//   if(index < stan.length) {
+//     containerE.innerHTML += stan[index] + '';
+//     index++;
+//     setTimeout(kiss, 200);
+
+//     console.log(containerE);
+
+//   }
+// }
+// console.log(stan[0]); 
+
+
+const containerE = document.querySelector('.container');
+
+const sports = ['Football', 'Basketball', 'Volleyball', 'Table tenis', 'and Swimming.'];
 
 let index = 0;
-const containerE = document.querySelector('.cont');
-const stan = `Thank you lord for providing for me`
+let characterIndex = 0;
 
-kiss();
+generateSports();
 
-function kiss() {
-  if(index < stan.length) {
-    containerE.innerHTML += stan[index] + '';
+function generateSports() {
+  characterIndex++;
+  containerE.innerHTML = `
+  <h1>My favourite sports are:<br><span>${sports[index].slice(0, characterIndex)}</span></h1>
+`;
+
+
+  if(characterIndex === sports[index].length) {
     index++;
-    setTimeout(kiss, 200);
+    characterIndex = 0;
 
-    console.log(containerE);
+    if(index === sports.length) {
+      // index = 0;
+    }
 
   }
+
+  setTimeout(generateSports, 400);
+
+
 }
-console.log(stan[0]); 
+
